e47127a1e66cfc7ae3234cd3b268685a50ceea62
Marc Riera <mrc2407@gmail.com>
2015-01-30 15:19:41 +0100

    Improve drawer styles

--- app/src/main/java/com/codegram/conferences/fullstackfest/ListActivity.java
+++ app/src/main/java/com/codegram/conferences/fullstackfest/ListActivity.java
@@ -1,6 +1,6 @@
 package com.codegram.conferences.fullstackfest;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
@@ -9,10 +9,22 @@ public class ListActivity extends MaterialNavigationDrawer {
 
     @Override
     public void init(Bundle savedInstanceState) {
-        //this.setDrawerHeaderImage(R.drawable.mat2);
+        // Set custom background image for the header
+        this.setDrawerHeaderImage(R.drawable.mat3);
+
+        // to disable the menu opening every time we open the app
+        // this.disableLearningPattern();
 
         //create sections
-        this.addSection(newSection("Schedule", new TalkListFragment()));
-        this.addSection((newSection("Speakers", new SpeakerListFragment())));
+        this.addSection(newSection("Schedule", new TalkListFragment()).setSectionColor(Color.parseColor("#9c27b0")));
+        this.addSection((newSection("Speakers", new SpeakerListFragment())).setSectionColor(Color.parseColor("#03a9f4")));
+
+        // ---------------------------------------------------
+        // set a subheader
+        this.addSubheader("Subheader 2");
+
+        //create sections
+        this.addSection(newSection("Schedule", new TalkListFragment()).setSectionColor(Color.parseColor("#9c27b0")));
+        this.addSection((newSection("Speakers", new SpeakerListFragment())).setSectionColor(Color.parseColor("#03a9f4")));
     }
 }
