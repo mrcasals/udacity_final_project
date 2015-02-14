2e154a984ede9427f80d4cb89d9bb1283080f30d
Marc Riera <mrc2407@gmail.com>
2015-02-14 18:30:56 +0100

    Show venue on Google Maps app

--- app/src/main/java/com/codegram/conferences/fullstackfest/config/FullStackFestConfig.java
+++ app/src/main/java/com/codegram/conferences/fullstackfest/config/FullStackFestConfig.java
@@ -7,4 +7,5 @@ public class FullStackFestConfig {
     public static final String BARUCO_COLOR = "#FFC107";
     public static final String FUTUREJS_COLOR = "#009688";
     public static final String API_ENDPOINT = "http://conferences.codegram.com/api/baruco2014/talks/";
+    public static final String LATLONG = "41.389280,2.137002";
 }
