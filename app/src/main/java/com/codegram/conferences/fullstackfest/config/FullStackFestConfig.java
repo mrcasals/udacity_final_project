2edfa2137904ca45fe475d5df599db04cf42feb7
Marc Riera <mrc2407@gmail.com>
2015-03-10 11:27:48 +0100

    Use FullStackFest data

--- app/src/main/java/com/codegram/conferences/fullstackfest/config/FullStackFestConfig.java
+++ app/src/main/java/com/codegram/conferences/fullstackfest/config/FullStackFestConfig.java
@@ -6,7 +6,7 @@ package com.codegram.conferences.fullstackfest.config;
 public class FullStackFestConfig {
     public static final String BARUCO_COLOR = "#FFC107";
     public static final String FUTUREJS_COLOR = "#009688";
-    public static final String API_ENDPOINT = "http://conferences.codegram.com/api/baruco2014/talks/";
+    public static final String API_ENDPOINT = "http://conferences.codegram.com/api/full-stack-fest/talks/";
     public static final String LATLONG = "41.389280,2.137002";
     public static final String VENUE_NAME = "Auditori AXA";
 }
