dddaee0d34ad32b63a08634cad12d036c030c81a
Marc Riera <mrc2407@gmail.com>
2015-04-08 14:20:52 +0200

    Receive push notifications

---
+++ app/src/main/java/com/codegram/conferences/fullstackfest/CustomReceiver.java
@@ -0,0 +1,35 @@
+package com.codegram.conferences.fullstackfest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.codegram.conferences.fullstackfest.tasks.FetchDataTask;
+import com.parse.Parse;
+import com.parse.ParsePushBroadcastReceiver;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by marcriera on 4/8/15.
+ */
+public class CustomReceiver extends ParsePushBroadcastReceiver {
+    @Override
+    public void onPushReceive(Context context, Intent intent) {
+        JSONObject pushData = null;
+        try {
+            pushData = new JSONObject(intent.getStringExtra("com.parse.Data"));
+        } catch (JSONException e) {
+            Log.e("CustomReceiver", "Unexpected JSONException when receiving push data: ", e);
+        }
+
+        if(pushData != null && pushData.has("customAction")) {
+            Log.d("CustomReceiver", "custom action!");
+            FetchDataTask task = new FetchDataTask(context);
+            task.execute();
+        } else {
+            super.onPushReceive(context, intent);
+        }
+    }
+}
--- app/src/main/java/com/codegram/conferences/fullstackfest/MyApplication.java
+++ app/src/main/java/com/codegram/conferences/fullstackfest/MyApplication.java
@@ -1,8 +1,12 @@
 package com.codegram.conferences.fullstackfest;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 /**
  * Created by marcriera on 4/8/15.
@@ -10,5 +14,15 @@ import com.parse.Parse;
 public class MyApplication extends Application {
     public void onCreate() {
         Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 }
